9a87b307be342a3e2591d648cbe9ff3be15297b0
Gradyn Wursten <me@gradyn.com>
2023-03-08 17:03:55 -0700

    add hibernate models

---
+++ src/main/java/com/gradyn/McsMigrationManager/Data/Models/Plot.java
@@ -0,0 +1,75 @@
+package com.gradyn.McsMigrationManager.Data.Models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Entity
+@Table(name="plot")
+public class Plot {
+    @Id
+    @Column(name="id")
+    private int id;
+    @Column(name="plot_id_x")
+    private int PlotIdX;
+    @Column(name="plot_id_z")
+    private int PlotIdZ;
+    @Column(name="owner")
+    private UUID Owner;
+    @Column(name="world")
+    private String world;
+    @Column(name="timestamp")
+    private Timestamp timestamp;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPlotIdX() {
+        return PlotIdX;
+    }
+
+    public void setPlotIdX(int plotIdX) {
+        PlotIdX = plotIdX;
+    }
+
+    public int getPlotIdZ() {
+        return PlotIdZ;
+    }
+
+    public void setPlotIdZ(int plotIdZ) {
+        PlotIdZ = plotIdZ;
+    }
+
+    public UUID getOwner() {
+        return Owner;
+    }
+
+    public void setOwner(UUID owner) {
+        Owner = owner;
+    }
+
+    public String getWorld() {
+        return world;
+    }
+
+    public void setWorld(String world) {
+        this.world = world;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+}
---
+++ src/main/java/com/gradyn/McsMigrationManager/Data/Models/UserCache.java
@@ -0,0 +1,31 @@
+package com.gradyn.McsMigrationManager.Data.Models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Table;
+
+import java.util.UUID;
+
+@Table(name="usercache")
+public class UserCache {
+    @Column(name="uuid")
+    private UUID UUID;
+
+    @Column(name="username")
+    private String Username;
+
+    public java.util.UUID getUUID() {
+        return UUID;
+    }
+
+    public void setUUID(java.util.UUID UUID) {
+        this.UUID = UUID;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+}
