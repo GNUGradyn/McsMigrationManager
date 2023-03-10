64f5f0cdb09e4df133b7df79de30f376eb555e38
Gradyn Wursten <me@gradyn.com>
2023-03-10 11:37:06 -0700

    fix acceptrules hopefully

--- src/main/java/com/gradyn/McsMigrationManager/commands/CommandAcceptRules.java
+++ src/main/java/com/gradyn/McsMigrationManager/commands/CommandAcceptRules.java
@@ -1,6 +1,7 @@
 package com.gradyn.McsMigrationManager.commands;
 
 import net.luckperms.api.LuckPermsProvider;
+import net.luckperms.api.node.Node;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,13 +12,14 @@ public class CommandAcceptRules implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
         var luckperms = LuckPermsProvider.get();
+        var player = (Player) commandSender;
+        var user = luckperms.getPlayerAdapter(Player.class).getUser(player);
         if (commandSender.hasPermission("group.constructor")) {
             commandSender.sendMessage("You already accepted the rules!");
             return true;
         }
-        var player = (Player) commandSender;
-        var user = luckperms.getUserManager().getUser(player.getUniqueId());
-        user.setPrimaryGroup("constructor");
+        user.data().add(Node.builder("group.constructor").build());
+        luckperms.getUserManager().saveUser(user);
         player.sendMessage("Thank you for accepting the rules! You are now a constructor!");
         return true;
     }
