3b7089f66b8758f998877ed06c1a37648df29480
Gradyn Wursten <me@gradyn.com>
2023-03-18 12:04:34 -0600

    ground work for archive command

---
+++ src/main/java/com/gradyn/McsMigrationManager/Data/Models/Archive.java
@@ -0,0 +1,58 @@
+package com.gradyn.McsMigrationManager.Data.Models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="archive")
+public class Archive {
+    @Id
+    @Column(name="id")
+    private String Id;
+
+    @Column(name="owner")
+    private String Owner;
+
+    @Column(name="x")
+    private int x;
+
+    @Column(name="y")
+    private int y;
+
+    @Column(name="z")
+    private int z;
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getOwner() {
+        return Owner;
+    }
+
+    public void setOwner(String owner) {
+        Owner = owner;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
